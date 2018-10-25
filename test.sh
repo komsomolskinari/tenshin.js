606e98db3257cda0432150851be5b573d77edd7e
Student Main <scncgz@gmail.com>
2018-10-25 15:21:08 +0800

    fix typo in test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 webpack --mode none src/index.js --output index.js
-mv node_modules/jquery/dist/jquery.min.js jquery.min.js
-mv src/index.html index.html
+cp node_modules/jquery/dist/jquery.min.js jquery.min.js
+cp src/index.html index.html
 #inkscape -z yuzumash.svg -e favicon.png
 #convert favicon.png -define icon:auto-resize=256,64,48,16 favicon.ico
 #sudo php -S 127.0.0.1:80
