ab831b39c52d4526b6e312eb4a8b6ee5e02653bc
Student Main <scncgz@gmail.com>
2018-10-26 16:24:43 +0800

    add fs tree generator, remove nginx dependency

---
+++ appendix/generate_tree_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os, sys, stat, json
+
+def gen_dir(path):
+    os.chdir(path)
+    ret = []
+    ds = os.listdir()
+    for d in ds:
+        j = {"name":d,"type":"file","sub":None}
+        if os.path.isdir(d):
+            j["type"] = "directory"
+            j["sub"] = gen_dir(d)
+        ret.append(j)
+    os.chdir('../')
+    return ret
+t = gen_dir(sys.argv[1])
+print(json.dumps(t,indent=4))
