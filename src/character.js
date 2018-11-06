0fc08e3e668d146c8f819059f1721649b0f26179
Student Main <scncgz@gmail.com>
2018-11-06 17:17:45 +0800

    class Character, independent TextHTML

---
+++ src/character.js
@@ -0,0 +1,229 @@
+import ObjectMapper from "./objmapper";
+import YZSound from "./ui/sound";
+import FilePath from "./utils/filepath";
+import KRCSV from "./utils/krcsv";
+const X = 0;
+const WIDTH = 0;
+const HORIZONTAL = 0;
+const Y = 1;
+const HEIGHT = 1;
+const VERTICAL = 1;
+export default class Character {
+    constructor(name) {
+        this.name = name;
+        this.currentVoice = 1;
+        this.nextVoice = undefined;
+        this.voiceFmt = ObjectMapper.GetNameInfo(name).voicefile;
+        this.displayName = ObjectMapper.GetNameInfo(name).standname;
+        // if character has no image, skip image meta
+        let fgLs = FilePath.ls(`fgimage/${name}`);
+        /*{
+        d1:{
+            1:[dname1,prefix1]
+        }}*/
+        this.dress = {};
+        /*{
+        prefix1:{
+            1:[fname1,fname2]
+        }}*/
+        this.face = {};
+        /*{
+        prefix1:{
+            variable3:{
+                layer4:{
+                    offset: [loffx, loffy],
+                    size: [lsizex, lsizey],
+                    layer: layerid
+                }
+            }
+        }}*/
+        this.coord = {};
+
+        this.imageLevel = 1;
+        this.imageXPos = 0;
+        this.dressOpt = "";
+        Character.characters[name] = this;
+        if (fgLs === undefined) return;
+        Object.keys(fgLs).filter(f => f.match(/info\.txt$/)).forEach(f => this.__LoadChunk(f));
+        Object.keys(fgLs).filter(f => f.match(/[0-9]\.txt$/)).forEach(f => this.__LoadCoord(f));
+
+    }
+
+    async __LoadChunk(filename) {
+        const f = KRCSV.Parse(await $.get(FilePath.find(filename)), '\t', false);
+        const _fsp = filename.split('_');
+        const pfx = _fsp.slice(1, _fsp.length - 1).join('_');
+
+        if (this.face[pfx] === undefined)
+            this.face[pfx] = {};
+
+        f.filter(l => l.length == 5).forEach(l => {
+            let [, dname, , dno, dvstr] = l;
+            if (this.dress[dname] === undefined)
+                this.dress[dname] = {};
+            this.dress[dname][dno] = [dvstr, pfx];
+        })
+        f.filter(l => l.length == 4).forEach(l => {
+            let [, fno, , fvstr] = l;
+            if (this.face[pfx][fno] == undefined)
+                this.face[pfx][fno] = [];
+            this.face[pfx][fno].push(fvstr);
+        })
+    }
+
+    async __LoadCoord(filename) {
+        let f = KRCSV.Parse(await $.get(FilePath.find(filename)), '\t')
+        const fvar = filename.match(/_([0-9])\./)[1];
+        const _fsp = filename.split('_');
+        const pfx = _fsp.slice(1, _fsp.length - 1).join('_');
+
+        if (this.coord[pfx] === undefined)
+            this.coord[pfx] = {};
+        if (this.coord[pfx][fvar] === undefined)
+            this.coord[pfx][fvar] = {};
+
+        f.forEach(l => {
+            const [, lname, loffx, loffy, lsizex, lsizey, , , , lid] = l;
+            this.coord[pfx][fvar][lname] = {
+                offset: [loffx, loffy],
+                size: [lsizex, lsizey],
+                layer: lid
+            };
+        })
+    }
+    //
+    // o- nextvoice: generate a new voice channel, play voice, delete it
+    // o- stopvoice: ?
+    // op bvoice: set to bvoice channel/ clear bvoice channel
+    // -p [x]voice: modify next voice, if number, change curVoice
+    // -p [?]delayrun: do it when voice play to arg
+    // o- [?]sync: sync with what?
+    Process(cmd) {
+        let { name, option, param } = cmd;
+        if (name !== this.name) return;
+
+        // delayrun: just set a delay to timeline
+        // TODO: use eventlistener to channel instead of cmd
+        // or calculate
+        if (param.delayrun) {
+            delete cmd.param.delayrun;
+            setTimeout(() => this.Process(cmd), 1000);
+            return;
+        }
+
+        // first, voice option:
+        if (param.voice) {
+            if (!isNaN(parseInt(param.voice))) {
+                this.currentVoice = parseInt(param.voice);
+            }
+            else {
+                this.nextVoice = param.voice;
+            }
+        }
+
+
+        // if standName !== name, we need call another character's Image()
+        // TODO: Update imageLevel, imageXPos
+        // TODO: Select faceOpt
+
+        let allDress = Object.keys(this.dress);
+        let dOpt = option.filter(o => allDress.includes(o))[0];
+        if (dOpt) {
+            this.dressOpt = dOpt;
+        }
+        let fOpt = option.filter(o => o.match(/^[0-9]{3}$/) != null)[0];
+        if (fOpt) {
+            this.Image(fOpt);
+        }
+    }
+
+    Text(line) {
+
+    }
+
+    /**
+     * 
+     * @param {*} seq Alternate seq id, only apply non-nuber
+     */
+    Voice(seq) {
+        this.nextVoice = undefined;
+        let s;
+        if (isNaN(parseInt(seq)) && seq !== undefined) {
+            s = seq;
+        }
+        else { // number or undefined, ignore arg seq
+            s = this.currentVoice;
+            this.currentVoice++;
+        }
+        let stxt = s;
+        if (!isNaN(parseInt(s))) {
+            stxt = this.voiceFmt;
+            // TODO: real printf
+            stxt = stxt
+                .replace('%s', Character.voiceBase)
+                .replace('%03d', String(s).padStart(3, '0'));
+        }
+        // drop extension
+        stxt = stxt.replace(/\.[a-z0-9]{2,5}$/i, '');
+        console.log(stxt)
+        //YZSound.Voice(s);
+    }
+
+    Image(faceOpt) {
+        // select image
+        let mainId = parseInt(faceOpt.substr(0, 1))
+        let varId = parseInt(faceOpt.substr(1, 2))
+
+        let [mainImg, pfx] = this.dress[this.dressOpt][mainId];
+        let varImg = this.face[pfx][varId];
+        if (varImg === undefined) return;
+        // 35 50 75 100 120 140 bgexpand original
+        const usedVer = ([1, 1, 3, 3, 3, 5, 3])[this.imageLevel];
+
+        let raw = varImg.map(v => this.coord[pfx][usedVer][v]);
+        raw.push(this.coord[pfx][usedVer][mainImg]);
+        // conv name
+        raw = raw.map(v => {
+            return {
+                layer: ([this.name, pfx, usedVer, v.layer].join('_')),
+                offset: v.offset,
+                size: v.size
+            }
+        });
+
+        let baseSize = this.coord[pfx][usedVer]['null'].size;
+        // calculate coord
+        let scaleo = ObjectMapper.innerobj.levels[this.imageLevel];
+        let zoom = scaleo.imgzoom / 100;
+        if (this.imageLevel < 2) zoom = scaleo.zoom * 1.33 / 100;
+        // zoom each layer
+        raw = raw.map(v => {
+            let [ox, oy] = v.offset;
+            let [sx, sy] = v.size;
+            return {
+                layer: v.layer,
+                offset: [ox * zoom, oy * zoom],
+                size: [sx * zoom, sy * zoom]
+            }
+        })
+        // move to center
+        let [bx, by] = baseSize;
+        let baseOffset = [(1280 - bx) / 2, (960 - by) / 2];
+        baseOffset[HORIZONTAL] += this.imageXPos;
+        // manually set ypos, do we really need it? 
+        // (envinit.tjs).yoffset = "1200"
+        if (this.imageLevel > 1) baseOffset[VERTICAL] -= (240 + 2 * parseInt(scaleo.zoom));
+
+        let ctl = {
+            base: {
+                size: baseSize,
+                offset: baseOffset
+            },
+            layer: raw
+        };
+        console.log(ctl);
+    }
+}
+Character.voiceBase = "";
+Character.characters = {}
+window.Character = Character
---
+++ src/utils/texthtml.js
@@ -0,0 +1,52 @@
+/**
+ * convert text with ks format cmd to html
+ * @param {String} txt Text with KAG tag
+ * @returns {String} KAG tag converted to html tag
+ */
+export default function TextHTML(txt) {
+    if (txt.indexOf('[') < 0) return txt;
+    // first, cut to lines: text\n[cmd]\ntext
+    const t = txt
+        .replace(/\[/g, '\n[')
+        .replace(/\]/g, ']\n')
+        .split('\n');
+    // generate raw text and cmd position
+    let rs = "";
+    // use KSParser to parse function
+    let c = [];
+    t.forEach(e => {
+        if (e[0] == '[') {
+            let f = KSParser.Parse(e)[0];
+            c.push([rs.length, f])
+        }
+        else {
+            rs += e;
+        }
+    })
+    let p = 0;
+    let ret = "";
+    c.forEach(t => {
+        let f;
+        // append unformatted txt
+        ret += rs.substr(p, t[0] - p);
+        [p, f] = t;
+        switch (f.name) {
+            case 'ruby': // [ruby text='text']c
+                ret += '<ruby>';
+                ret += rs[p];
+                p++;
+                ret += '<rt>';
+                ret += f.param.text;
+                ret += '</rt></ruby>';
+                break;
+            case 'r':  // [r]
+                ret += '<br />';
+                break;
+            default:
+                console.warn("TextHTML, Unknown inline tag", t);
+                break;
+        }
+    });
+    ret += rs.substr(p);
+    return ret;
+}
