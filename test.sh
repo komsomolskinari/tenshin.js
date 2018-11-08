49f6587ef53780945eeb822def73509ab049e3cf
Student Main <scncgz@gmail.com>
2018-11-08 11:35:50 +0800

    use data url image as mask

--- test.sh
+++ test.sh
@@ -5,7 +5,6 @@ webpack --mode none src/index.js --output index.js
 cp node_modules/jquery/dist/jquery.min.js jquery.min.js
 cp src/index.html index.html
 cp src/index.css index.css
-cp src/1280.png 1280.png
 #inkscape -z yuzumash.svg -e favicon.png
 #convert favicon.png -define icon:auto-resize=256,64,48,16 favicon.ico
 #sudo php -S 127.0.0.1:80
