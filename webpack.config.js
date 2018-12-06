24886198f59f9eab8f654214975594a6b9c89c4a
Student Main <scncgz@gmail.com>
2018-12-06 13:17:47 +0800

    Migrate to Typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,23 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'index.js',
-        path: '/mnt/c/Users/User/Desktop/YUZUSOFT/tenshin_js'
-    },
     devtool: 'source-map',
     mode: 'none',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname)
+    }
 };
