9d815259ccbd33e097acdaa8ccb75f7585904f3b
Student Main <scncgz@gmail.com>
2018-12-13 22:39:11 +0800

    Update webpack config

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 npx webpack
-cp node_modules/jquery/dist/jquery.min.js jquery.min.js
-cp src/index.html index.html
-cp src/index.css index.css
+cd dist
+ln -s ../game
+cd ..
--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,31 @@ const path = require('path');
 module.exports = {
     devtool: 'source-map',
     mode: 'none',
-    entry: './src/index.ts',
+    entry: [
+        './src/index.ts',
+        './src/index.html',
+        './src/index.css',
+        './node_modules/jquery/dist/jquery.min.js',
+        './src/config.js'
+    ],
     module: {
         rules: [
             {
-                test: /\.tsx?$/,
+                test: /\.ts$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.(html|css|js)$/,
+                use: "file-loader?name=[name].[ext]"
             }
         ]
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js']
+        extensions: ['.ts', '.js']
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname)
+        path: path.resolve(__dirname) + '/dist'
     }
 };
