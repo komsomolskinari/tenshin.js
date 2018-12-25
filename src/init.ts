0fbad0b27fd73a5879acc29b6339a34d00c5a09c
Student Main <scncgz@gmail.com>
2018-12-25 16:25:47 +0800

    JS getMonth starts from 0...

--- src/init.ts
+++ src/init.ts
@@ -8,8 +8,9 @@ import FilePath from "./utils/filepath";
 
 export default async function Init() {
     // see: https://github.com/ant-design/ant-design/issues/13836
+    // the value from Date.getMonth() starts with 0
     const _EasterEggDate = new Date();
-    if (_EasterEggDate.getDate() === 17 && _EasterEggDate.getMonth() === 8) {
+    if (_EasterEggDate.getDate() === 17 && _EasterEggDate.getMonth() === 7) {
         alert("🐸🐸🐸");
     }
     await FilePath.Load();
