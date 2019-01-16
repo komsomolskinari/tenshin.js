88d6ea51d7f4a56678a7e79fa94562512524c86c
Student Main <scncgz@gmail.com>
2019-01-16 14:59:44 +0800

    Adjust build config

--- test.sh
+++ test.sh
@@ -5,4 +5,7 @@ cd dist
 # for windows/wsl
 # mklink /d game ..\game
 ln -s ../game
+cp src/index.html .
+cp src/index.css .
+cp src/config.js .
 cd ..
--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,13 @@ const path = require('path');
 module.exports = {
     devtool: 'source-map',
     mode: 'none',
-    entry: [
-        './src/index.ts',
-        './src/index.html',
-        './src/index.css',
-        './src/config.js'
-    ],
+    entry: './src/index.ts',
     module: {
         rules: [
             {
                 test: /\.ts$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
-            },
-            {
-                test: /\.(html|css|js)$/,
-                use: "file-loader?name=[name].[ext]"
             }
         ]
     },
